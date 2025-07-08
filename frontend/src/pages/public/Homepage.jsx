7359cfb408b9d13b5d8ee26f6dbf0aefaa0811e1
goraydev <jofre1236@gmail.com>
2025-07-08 16:42:50 -0500

    Mostrando boton para iniciar las preguntas

--- frontend/src/pages/public/Homepage.jsx
+++ frontend/src/pages/public/Homepage.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router";
+
 export const HomePage = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold mb-8 text-center">
+      <h1 className="text-4xl font-bold mb-8 text-center text-black">
         Bienvenido a la aplicación de Test Vocacional UNASAM
       </h1>
+      <Link to={"/preguntas"} className="btn inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium h-10 px-4 py-2 text-white border-0 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-600 hover:to-blue-600 shadow-lg shadow-cyan-500/25 transition-color">
+        Empezar cuestionario
+      </Link>
     </div>
   );
 };
